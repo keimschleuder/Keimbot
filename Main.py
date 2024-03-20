84d850db3b55192f103e68002113fde9f8e43623
Niklas <138971666+keimschleuder@users.noreply.github.com>
2024-03-20 16:54:54 +0100

    BAckup

--- Main.py
+++ Main.py
@@ -1,13 +1,22 @@
-from Token.Token import TOKEN as token
+import Token.Token as Token
 import discord
+from discord import app_commands
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix = "!", intents = intents)
 
 @client.event
 async def on_ready():
+    try: # Try to sync slash commands
+        synced = await bot.tree.sync(guild=discord.Object(id=Token.ID))
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
+    
     print(f'We have logged in as {client.user}')
 
 @client.event
@@ -15,7 +24,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
-client.run(token)
+@bot.tree.command(name = "ping")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong")
+
+client.run(Token.TOKEN)
