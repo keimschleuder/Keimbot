ec46d3592b08c7de1a7cbb82fb807f7a7b9ceaa1
Niklas <138971666+keimschleuder@users.noreply.github.com>
2024-03-20 17:21:13 +0100

    Update Main.py
    Commands Working, now preparing to make a custom voice activity

--- Main.py
+++ Main.py
@@ -5,21 +5,16 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents = intents)
-bot = commands.Bot(command_prefix = "!", intents = intents)
+activity = discord.Activity(type=discord.ActivityType.watching, name="your fingers")
+bot = commands.Bot(command_prefix = "!", activity=activity, intents = intents, status=discord.Status.online)
 
-@client.event
+@bot.event
 async def on_ready():
-    
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
 @bot.command()
 async def ping(ctx):
@@ -29,5 +24,4 @@ async def ping(ctx):
 async def wave(ctx, to: discord.User = commands.Author):
     await ctx.send(f'Hello {to.mention} :wave:')
 
-
-client.run(Token.TOKEN)
+bot.run(Token.TOKEN)
